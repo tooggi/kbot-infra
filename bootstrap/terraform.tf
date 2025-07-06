37689c4b21437d1f8d762cfb97e7da2d83b77d7c
Eugene Tugolukov <tooggi@gmail.com>
2025-07-06 18:14:51 +0200

    Added github repo and flux bootstrap

--- bootstrap/terraform.tf
+++ bootstrap/terraform.tf
@@ -7,6 +7,16 @@ terraform {
       version = ">= 0.9.0"
     }
 
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.6.0"
+    }
+
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 1.6.3"
+    }
+
     helm = {
       source  = "hashicorp/helm"
       version = ">= 3.0.2"
@@ -17,11 +27,34 @@ terraform {
 provider "kind" {}
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host = kind_cluster.local.endpoint
 
     client_certificate     = kind_cluster.local.client_certificate
     client_key             = kind_cluster.local.client_key
     cluster_ca_certificate = kind_cluster.local.cluster_ca_certificate
+  }
+}
+
+provider "github" {
+  owner = var.github_org
+  token = var.github_token
+}
+
+provider "flux" {
+  kubernetes = {
+    host = kind_cluster.local.endpoint
+
+    client_certificate     = kind_cluster.local.client_certificate
+    client_key             = kind_cluster.local.client_key
+    cluster_ca_certificate = kind_cluster.local.cluster_ca_certificate
+  }
+
+  git = {
+    url = "https://github.com/${var.github_org}/${var.github_repository}.git"
+    http = {
+      username = var.github_username
+      password = var.github_token
+    }
   }
 }
---
+++ bootstrap/variables.tf
@@ -0,0 +1,28 @@
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+  default     = "tooggi"
+}
+
+variable "github_repository" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "demo-flux"
+}
+
+variable "github_username" {
+  description = "GitHub username for authentication"
+  type        = string
+  default     = "flux-user"
+}
+
+variable "github_token" {
+  description = "GitHub token for authentication"
+  type        = string
+}
+
+variable "target_path" {
+  description = "Flux manifests subdirectory"
+  type        = string
+  default     = "clusters/local"
+}
